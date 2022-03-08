c29dec173c5f95e3cf08dfcf8d3b978d8c3b6e4c
prireds174 <pri.reds174@gmail.com>
2022-03-08 17:08:46 -0500

    Classes Added & Templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Home.as_view(), name="home")
+    path('', views.Home.as_view(), name="home"),
+    path('about/', views.About.as_view(), name="about"),
+    path('beers/', views.BeerList.as_view(), name="beer_list")
     
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,25 @@
+from ast import BinOp
+from re import template
 from django.shortcuts import render
-from django.
+from django.views import View
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+class Home(TemplateView):
+    template_name = "home.html"
+    # def get(self, request):
+    #     return HttpResponse("Smooth Hoperator")
+
+class About(View):
+
+    def get(self, request):
+        return HttpResponse("Smooth Hoperator About")
+
+class Beers:
+    def __init__(self, name, image, type, description):
+        self.name = name
+        self.image = image
+        self.type = type
+        self.description = description
