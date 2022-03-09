485e1890531a25ca3799fc4c2709e8112f7428af
prireds174 <pri.reds174@gmail.com>
2022-03-09 17:42:17 -0500

    Icons Clickable/Details Page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
-    path('beers/', views.BeerList.as_view(), name="beer_list")
+    path('beers/', views.BeerList.as_view(), name="beer_list"),
+    path('beers/<int:pk>/', views.BeerDetail.as_view(), name="beer_detail")
     
 
 ]
