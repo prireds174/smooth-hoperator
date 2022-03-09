956f2d3d59e98b1b913b9428391c27f63c3c9310
prireds174 <pri.reds174@gmail.com>
2022-03-09 11:23:38 -0500

    Added Static Data

--- main_app/views.py
+++ main_app/views.py
@@ -12,10 +12,8 @@ class Home(TemplateView):
     # def get(self, request):
     #     return HttpResponse("Smooth Hoperator")
 
-class About(View):
-
-    def get(self, request):
-        return HttpResponse("Smooth Hoperator About")
+class About(TemplateView):
+    template_name = "about.html"
 
 class Beer:
     def __init__(self, name, brand, image, type, description):
@@ -26,13 +24,13 @@ class Beer:
         self.description = description
 
 beers = [
-    Beer("Silent Accord", "Six Bridges Brewing", "https://www.ajc.com/resizer/Do3KCvCWpdvpyOVZrk4gYm0NtrY=/814x458/cloudfront-us-east-1.images.arcpublishing.com/ajc/XRYGDKSF36SEKERV7VLENYYELY.jpg" "Coconut Milk Stout", "Jet black in color with a creamy, tan head, this milk stout is full of flavors. Aromas of coffee and dark chocolate come through from the use of roasted barley and chocolate malts. Smooth, dark chocolate and subtle notes of toasted coconut create a pleasant bouquet of flavors for the palate. A medium-light body that shows creaminess and low roast makes this offering approachable year round. At 6.5%, this beer is a real treat for those looking for rich, velvety smooth stout." ),
+    Beer("Silent Accord", "Six Bridges Brewing", "https://www.ajc.com/resizer/Do3KCvCWpdvpyOVZrk4gYm0NtrY=/814x458/cloudfront-us-east-1.images.arcpublishing.com/ajc/XRYGDKSF36SEKERV7VLENYYELY.jpg", "Coconut Milk Stout", "Jet black in color with a creamy, tan head, this milk stout is full of flavors. Aromas of coffee and dark chocolate come through from the use of roasted barley and chocolate malts. Smooth, dark chocolate and subtle notes of toasted coconut create a pleasant bouquet of flavors for the palate. A medium-light body that shows creaminess and low roast makes this offering approachable year round. At 6.5%, this beer is a real treat for those looking for rich, velvety smooth stout." ),
     Beer("Neon Cylinders", "Creature Comforts Brewing Co.", "https://creaturecomfortsbeer.com/wp-content/uploads/2022/02/NeonCylinders-MagentaBeams_Can_for_web.jpg", "Sour Ale", "Neon Cylinders is an Intensely Fruited Sour Ale series. This version, 'Magenta Beams,' features prickly pear, passion fruit, Cara Cara orange, and grapefruit. This Neon Cylinders is rich in fruit flavor, and SUPER MAGENTA!" ),
     Beer("Bestie", "Wild Heaven Beer", "https://static.spotapps.co/web/wildheavenbeer--com/custom/download/bestie_logo.jpg", "Pub Ale", "Bestie is a delicious session Pub Ale brewed in collaboration with England’s Arundel Brewery, Wild Heaven Beer and Robert Merrick of Atlanta’s 9 Mile Station. Built on the backbone of a traditional Standard Pub Ale with our own twist added, Bestie is designed to be a flavor-forward, yet easy-drinking beer. It is medium amber in color with light hints of peat and dark fruits and finishes crisp and clean with lingering floral hops.")
 ]
 
 class BeerList(TemplateView):
-    template_name = "artist_list.html"
+    template_name = "beer_list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
