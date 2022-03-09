f5b777a981dade68b898f7cf732ce96beee8891d
prireds174 <pri.reds174@gmail.com>
2022-03-09 11:55:33 -0500

    Save before seed Data

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Beer(models.Model):
+    name = models.CharField(max_length=90)
+    brand = models.CharField(max_length=120)
+    img = models.CharField(max_length=250)
+    type = models.CharField(max_length=90)
+    desc = models.TextField(max_length=500)
+    currently_being_poured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+    
