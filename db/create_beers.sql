abd6b35adec235b94636bac34f2eadfdb2fa22b6
prireds174 <pri.reds174@gmail.com>
2022-03-15 11:17:15 -0400

    Update db file

--- db/create_beers.sql
+++ db/create_beers.sql
@@ -1,26 +1,26 @@
-INSERT INTO main_app_beer(name, brand, img, style, description, currently_being_poured, created_at)
+INSERT INTO main_app_beer(name, brand, img, style, description, currently_being_poured)
   VALUES
-    ('Silent Accord', 'Six Bridges Brewing', 'https://www.ajc.com/resizer/Do3KCvCWpdvpyOVZrk4gYm0NtrY=/814x458/cloudfront-us-east-1.images.arcpublishing.com/ajc/XRYGDKSF36SEKERV7VLENYYELY.jpg', 'Coconut Milk Stout', 'Jet black in color with a creamy, tan head, this milk stout is full of flavors. Aromas of coffee and dark chocolate come through from the use of roasted barley and chocolate malts. Smooth, dark chocolate and subtle notes of toasted coconut create a pleasant bouquet of flavors for the palate. A medium-light body that shows creaminess and low roast makes this offering approachable year round. At 6.5%, this beer is a real treat for those looking for rich, velvety smooth stout.', 't', '2022-03-09 12:18:10');
+    ('Silent Accord', 'Six Bridges Brewing', 'https://www.ajc.com/resizer/Do3KCvCWpdvpyOVZrk4gYm0NtrY=/814x458/cloudfront-us-east-1.images.arcpublishing.com/ajc/XRYGDKSF36SEKERV7VLENYYELY.jpg', 'Coconut Milk Stout', 'Jet black in color with a creamy, tan head, this milk stout is full of flavors. Aromas of coffee and dark chocolate come through from the use of roasted barley and chocolate malts. Smooth, dark chocolate and subtle notes of toasted coconut create a pleasant bouquet of flavors for the palate. A medium-light body that shows creaminess and low roast makes this offering approachable year round. At 6.5%, this beer is a real treat for those looking for rich, velvety smooth stout.', 't');
 
-INSERT INTO main_app_beer(name, brand, img, style, description, currently_being_poured, created_at)
+INSERT INTO main_app_beer(name, brand, img, style, description, currently_being_poured)
   VALUES
-    ('Bestie', 'Wild Heaven Beer', 'https://static.spotapps.co/web/wildheavenbeer--com/custom/download/bestie_logo.jpg', 'Pub Ale', 'Bestie is a delicious session Pub Ale brewed in collaboration with Englands Arundel Brewery, Wild Heaven Beer and Robert Merrick of Atlantas 9 Mile Station. Built on the backbone of a traditional Standard Pub Ale with our own twist added, Bestie is designed to be a flavor-forward, yet easy-drinking beer. It is medium amber in color with light hints of peat and dark fruits and finishes crisp and clean with lingering floral hops.', 't', '2022-03-09 12:20:20');
+    ('Bestie', 'Wild Heaven Beer', 'https://static.spotapps.co/web/wildheavenbeer--com/custom/download/bestie_logo.jpg', 'Pub Ale', 'Bestie is a delicious session Pub Ale brewed in collaboration with Englands Arundel Brewery, Wild Heaven Beer and Robert Merrick of Atlantas 9 Mile Station. Built on the backbone of a traditional Standard Pub Ale with our own twist added, Bestie is designed to be a flavor-forward, yet easy-drinking beer. It is medium amber in color with light hints of peat and dark fruits and finishes crisp and clean with lingering floral hops.', 't');
 
-INSERT INTO main_app_beer(name, brand, img, style, description, currently_being_poured, created_at)
+INSERT INTO main_app_beer(name, brand, img, style, description, currently_being_poured)
   VALUES
-    ('Neon Cylinders', 'Creature Comforts Brewing Co.', 'https://creaturecomfortsbeer.com/wp-content/uploads/2022/02/NeonCylinders-MagentaBeams_Can_for_web.jpg', 'Sour Ale', 'Neon Cylinders is an Intensely Fruited Sour Ale series. This version, Magenta Beams, features prickly pear, passion fruit, Cara Cara orange, and grapefruit. This Neon Cylinders is rich in fruit flavor, and SUPER MAGENTA!', 't', '2022-03-09 12:28:20');
+    ('Neon Cylinders', 'Creature Comforts Brewing Co.', 'https://creaturecomfortsbeer.com/wp-content/uploads/2022/02/NeonCylinders-MagentaBeams_Can_for_web.jpg', 'Sour Ale', 'Neon Cylinders is an Intensely Fruited Sour Ale series. This version, Magenta Beams, features prickly pear, passion fruit, Cara Cara orange, and grapefruit. This Neon Cylinders is rich in fruit flavor, and SUPER MAGENTA!', 't');
 
-INSERT INTO main_app_beer(name, brand, img, style, description, currently_being_poured, created_at)
+INSERT INTO main_app_beer(name, brand, img, style, description, currently_being_poured)
   VALUES
-    ('Oaxacan', 'Wicked Weed Brewing', 'https://craftpeak-cooler-images.imgix.net/wicked-weed-brewing/Oaxacan-1.jpg?auto=compress%2Cformat&fit=scale&h=1280&ixlib=php-1.2.1&w=1920&wpsize=full_width_photo&s=bf16607c644191a0dde9e5c2648d7219', 'Smoked Sour', 'Oaxacan begins as an amber sour ale brewed with smoked wheat malts. Its then laid to rest in Pink Tequila Barrels (red wine barrels used to age Tequila, lending a softer spirit character) with grapefruit and lime zest. When Oaxacan reaches its its peak complexity, its blended onto additional grapefruit to lend fresh citrus essence.', 'f', '2022-03-09 12:46:20');
+    ('Oaxacan', 'Wicked Weed Brewing', 'https://craftpeak-cooler-images.imgix.net/wicked-weed-brewing/Oaxacan-1.jpg?auto=compress%2Cformat&fit=scale&h=1280&ixlib=php-1.2.1&w=1920&wpsize=full_width_photo&s=bf16607c644191a0dde9e5c2648d7219', 'Smoked Sour', 'Oaxacan begins as an amber sour ale brewed with smoked wheat malts. Its then laid to rest in Pink Tequila Barrels (red wine barrels used to age Tequila, lending a softer spirit character) with grapefruit and lime zest. When Oaxacan reaches its its peak complexity, its blended onto additional grapefruit to lend fresh citrus essence.', 'f');
 
-INSERT INTO main_app_beer(name, brand, img, style, description, currently_being_poured, created_at)
+INSERT INTO main_app_beer(name, brand, img, style, description, currently_being_poured)
   VALUES
-    ('Lord Far-Quad', 'Reformation Brewery', 'https://untappd.akamaized.net/site/beer_logos_hd/beer-4693168_19709_hd.jpeg', 'Belgian Quadrupel', 'The comically short, ruthless ruler of Duloc... I mean... a delightful Belgian with stone fruit and caramel notes. Darker in color and still adjunct free. Hes biased & boozy.', 't', '2022-03-09 13:01:10');
+    ('Lord Far-Quad', 'Reformation Brewery', 'https://untappd.akamaized.net/site/beer_logos_hd/beer-4693168_19709_hd.jpeg', 'Belgian Quadrupel', 'The comically short, ruthless ruler of Duloc... I mean... a delightful Belgian with stone fruit and caramel notes. Darker in color and still adjunct free. Hes biased & boozy.', 't' );
 
-INSERT INTO main_app_beer(name, brand, img, style, description, currently_being_poured, created_at)
+INSERT INTO main_app_beer(name, brand, img, style, description, currently_being_poured)
   VALUES
-    ('Deadeye Dunkel', 'Pug Ryans', 'https://static.wixstatic.com/media/d81dee_4ecddca5a8204db796bda7a692971c3b~mv2_d_3300_2550_s_4_2.png/v1/crop/x_1169,y_588,w_877,h_1397/fill/w_337,h_537,al_c,usm_0.66_1.00_0.01,enc_auto/dunkel%202%20clear.png', 'Munich Dunkel Lager', 'A traditional Munich style dark lager, smooth malty flavor with a clean finish.', 't', '2022-03-09 13:13:13' );
+    ('Deadeye Dunkel', 'Pug Ryans', 'https://static.wixstatic.com/media/d81dee_4ecddca5a8204db796bda7a692971c3b~mv2_d_3300_2550_s_4_2.png/v1/crop/x_1169,y_588,w_877,h_1397/fill/w_337,h_537,al_c,usm_0.66_1.00_0.01,enc_auto/dunkel%202%20clear.png', 'Munich Dunkel Lager', 'A traditional Munich style dark lager, smooth malty flavor with a clean finish.', 't');
 
 
 
